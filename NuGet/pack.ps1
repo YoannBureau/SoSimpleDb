ad239c70bd44f6f745be5b7c102acd0b3180a665
Yoann BUREAU <ybureau@gmail.com>
2017-03-26 10:21:59 +0200

    Updated Assembly.info & appveyor.yml

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -1,7 +1,8 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 #$version = [System.Reflection.Assembly]::LoadFile("$root\SoSimpleDb\bin\$configuration\SoSimpleDb.dll").GetName().Version
 #$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-$versionStr = "{0}.{1}.{2}" -f (1, 0, 0)
+#$versionStr = "{0}.{1}.{2}" -f (1, 0, $env:APPVEYOR_BUILD_NUMBER)
+$versionStr = $env:APPVEYOR_BUILD_VERSION
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
--- SoSimpleDb/Properties/AssemblyInfo.cs
+++ SoSimpleDb/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SoSimpleDb")]
-[assembly: AssemblyCopyright("Copyright ©YoannBureau  2017")]
+[assembly: AssemblyCopyright("Copyright ©YoannBureau 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
