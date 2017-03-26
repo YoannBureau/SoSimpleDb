5eae30a1653f7269da171eb0f47dbcd814bba5fe
Yoann BUREAU <ybureau@gmail.com>
2017-03-26 09:38:39 +0200

    Nuget pack script for Appveyor

---
+++ NuGet/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\SoSimpleDb\bin\$configuration\SoSimpleDb.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\SoSimpleDb.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\NuGet\SoSimpleDb.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\NuGet\SoSimpleDb.compiled.nuspec
